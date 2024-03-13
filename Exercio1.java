079c51ff870919b4b04e0c9a76c188989aa71663
Auto Logon <autologon@CSED.LAB>
2024-03-13 10:04:14 -0300

    mais commit

---
+++ Exercio1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+/*1. Escreva um algoritmo que leia um número digitado pelo usuário e mostre amensagem “Número maior do que 10!”, caso este número seja maior, ou “Númeromenor ou igual a 10!”, caso este número seja menor ou igual*/
+
+public class Exercio1 {
+
+}
---
+++ Exercio2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*2. Escreva um algoritmo que leia dois números digitados pelo usuário e exiba oresultado da sua soma. */
+
+public class Exercio2 {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int n1 = Integer.parseInt(leitor.nextLine());
+
+        System.out.println("Digite o segundo número: ");
+        int n2 = Integer.parseInt(leitor.nextLine());
+
+        int soma = n1 + n2;
+
+        System.out.println("A soma dos dois valores é: " + soma);
+  }
+    }
