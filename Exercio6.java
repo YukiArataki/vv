9b703e24ba14677e1b5c753cb13ccb20cfbf5080
Auto Logon <autologon@CSED.LAB>
2024-03-14 09:45:19 -0300

    outro

--- Exercio6.java
+++ Exercio6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/*6.Ler uma temperatura em graus Celsius e apresentá-la convertida em grausFahrenheit. 
+/*6.Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
 A fórmula de conversão é: F = (9 * C + 160) /5 */
 
 public class Exercio6 {
@@ -8,8 +8,16 @@ public class Exercio6 {
     public static void main(String[] args) {
         
         Scanner leitor = new Scanner(System.in);
+        double Celsius, Fahrenheit;
 
-        System.out.print("Conversor de temperatura: ");
+        System.out.print("Conversor de temperatura: Graus Celsios -> Fahrenheit\n\n");
+
+        System.out.println("Digite a temperatura em Celsius: ");
+        Celsius = leitor.nextDouble();
+
+        Fahrenheit = (9 * Celsius + 160) / 5;
+
+        System.out.print("\n A media convertida é " + Fahrenheit + "ºF\n");
     }
 
 }
