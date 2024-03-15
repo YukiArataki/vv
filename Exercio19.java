ca7afa987db6fab501f24b5e07768f8823a75d6f
Auto Logon <autologon@CSED.LAB>
2024-03-15 09:53:09 -0300

    O commit

--- Exercio19.java
+++ Exercio19.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /*19. Faça um algoritmo que receba o raio e a altura de um cilindro e retorne o seuvolume 
-calculado de acordo com a seguinte fórmula: volume = 3.14 * raio2 * altura;Exemplo: raio = 10, altura = 15. 
+calculado de acordo com a seguinte fórmula: volume = 3.14 * raio2 * altura; Exemplo: raio = 10, altura = 15. 
 Volume = 4710 */
 
 public class Exercio19 {
@@ -9,6 +9,21 @@ public class Exercio19 {
     public static void main(String[] args) {
         
         Scanner leitor = new Scanner(System.in); 
+
+        Double volume;
+        Double v = 3.14;
+        Double r;
+        Double altura;
+
+        System.out.println("Informe o raio da circuferencia");
+        r = leitor.nextDouble();
+
+        System.out.println("Informe a altura");
+        altura = leitor.nextDouble();
+
+        volume = v * (r*r) * altura;
+
+        System.out.println("O valor do volume de lata de óleo é" + volume);
     }
 
 }
