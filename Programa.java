18b24ab8b7c1bc2c7d7e8b5c02e206f7b9283bca
Auto Logon <autologon@CSED.LAB>
2024-03-20 10:50:23 -0300

    mais

--- Programa.java
+++ Programa.java
@@ -2,7 +2,16 @@ public class Programa {
 
     public static void main(String[] args){
 
-        Exercio4.executar();
+        //Exercio1.executar();
+        //Exercio2.executar();
+        //Exercio4.executar();
+        //Exercio5.executar();
+        //Exercio6.executar();
+        //Exercio7.executar();
+        //Exercio11.executar();
+        //Exercio12.executar();
+        //Exercio19.executar();
+        Exercio20.executar();
 
     }
 
